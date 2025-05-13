4af2d662e15822a6b8840770aceccd6f5d4ffe1f
fj2lin <63832778+fj2lin@users.noreply.github.com>
2025-05-13 20:20:27 -0300

    gcs total somado funcionando

--- js/main.js
+++ js/main.js
@@ -31,6 +31,33 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+document.querySelectorAll('.gcs').forEach(response=>{
+  response.querySelectorAll('.option').forEach(option=>{
+    //document.getElementById('gcs-result').textContent = 'got to optsion';
+    option.addEventListener('click', ()=>{
+      //document.getElementById('gcs-result').textContent = 'event listener added';
+      recalculateGCS();
+    })
+  })
+})
+
+function recalculateGCS(){
+  let gcs = 0;
+  let nonTestable = false;
+  document.querySelectorAll('.option.selected').forEach(selectedOption=>{
+    if (selectedOption.dataset.value === 'NT'){
+      nonTestable = true;
+    } else{
+      gcs += parseInt(selectedOption.dataset.value, 10);
+    }
+  });
+  if(nonTestable){
+    document.getElementById('gcs-result').textContent = gcs.toString() + 'NT';
+  } else {
+    document.getElementById('gcs-result').textContent = gcs;
+  }
+}
+
 // RASS - Display value
 const selectRass = document.getElementById('rass-select');
 const output = document.getElementById('rass-result');
